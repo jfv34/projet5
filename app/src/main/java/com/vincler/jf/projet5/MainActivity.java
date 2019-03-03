71301ff562fdb90de8b56ff14b775efb21381a41
JFV34 <jfr.vincler@live.fr>
2019-03-03 21:04:00 +0100

    added toolbar

--- app/src/main/java/com/vincler/jf/projet5/MainActivity.java
+++ app/src/main/java/com/vincler/jf/projet5/MainActivity.java
@@ -1,7 +1,9 @@
 package com.vincler.jf.projet5;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.configureToolbar();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_activity_main, menu);
+        return true;
+    }
+
+
+    private void configureToolbar() {
+        Toolbar toolbar = findViewById(R.id.activity_main_toolbar);
+        setSupportActionBar(toolbar);
     }
 }
