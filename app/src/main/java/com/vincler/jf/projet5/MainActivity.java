b60216ef34bf72700baec7426d38ff635f72c473
JFV34 <jfr.vincler@live.fr>
2019-03-10 19:06:27 +0100

    begun the DrawerLayout

--- app/src/main/java/com/vincler/jf/projet5/MainActivity.java
+++ app/src/main/java/com/vincler/jf/projet5/MainActivity.java
@@ -1,20 +1,65 @@
 package com.vincler.jf.projet5;
 
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         configureToolbar();
         configureViewPagerAndTabs();
+        configureDrawerLayout();
+        configureNavigationView();
+    }
+
+    @Override
+
+    public void onBackPressed() {
+
+        if (this.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            this.drawerLayout.closeDrawer(GravityCompat.START);
+
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
+    public boolean onNavigationItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.activity_main_drawer_news:
+                break;
+            case R.id.activity_main_drawer_profile:
+                break;
+            case R.id.activity_main_drawer_settings:
+                break;
+            default:
+                break;
+        }
+
+
+        this.drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     @Override
@@ -25,7 +70,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void configureToolbar() {
-        Toolbar toolbar = findViewById(R.id.activity_main_toolbar);
+        this.toolbar = findViewById(R.id.activity_main_toolbar);
         setSupportActionBar(toolbar);
     }
 
@@ -38,4 +83,15 @@ public class MainActivity extends AppCompatActivity {
         tabs.setTabMode(TabLayout.MODE_FIXED);
     }
 
+    private void configureDrawerLayout() {
+        this.drawerLayout = findViewById(R.id.activity_main_drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    private void configureNavigationView() {
+        //this.navigationView = findViewById(R.id.activity_main_nav_view);
+        //navigationView.setNavigationItemSelectedListener(this);
+    }
 }
